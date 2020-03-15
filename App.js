1134b95f5e90dc74dd52453a6ee48c41761143c8
kevin <chengyu0718@hotmail.com>
2020-03-14 21:40:29 -0700

    header is now done

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import MainPage from './Components/MainPage';
 
-export default function App() {
-	return <MainPage />;
-}
+const App = () => {
+	return (
+		<View>
+			<MainPage />
+		</View>
+	);
+};
 
-const styles = StyleSheet.create({});
+export default App;
--- Components/MainPage.js
+++ Components/MainPage.js
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity, Image } from 'react-native';
 import backGroundImage1 from '../assets/food-background-1.jpg';
 
 const Header = () => {
 	return (
 		<ImageBackground source={backGroundImage1} style={styles.backgroundImg}>
-			<View style={styles.view} />
+			<View style={styles.view}>
+				<TouchableOpacity style={[ styles.button, { backgroundColor: 'rgba(25, 190, 63, 0.89)' } ]}>
+					<Image source={require('../assets/spoon-512.png')} style={styles.buttonImg} />
+					<Text style={styles.buttonText}>外卖</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={[ styles.button, { backgroundColor: 'rgba(251, 191, 50, 0.89)' } ]}>
+					<Image source={require('../assets/bag.png')} style={[ styles.buttonImg, { marginLeft: 11 } ]} />
+					<Text style={styles.buttonLongText}>到店自取</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={[ styles.button, { backgroundColor: 'rgba(23, 191, 207, 0.9)' } ]}>
+					<Image source={require('../assets/shop.png')} style={[ styles.buttonImg, { marginLeft: 10 } ]} />
+					<Text style={styles.buttonLongText}>百货商场</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={[ styles.button, { backgroundColor: 'rgba(247, 78, 75, 0.9)' } ]}>
+					<Image
+						source={require('../assets/onigiri-512.png')}
+						style={[ styles.buttonImg, { marginLeft: 10 } ]}
+					/>
+					<Text style={styles.buttonLongText}>饭团点评</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={[ styles.button, { backgroundColor: 'rgba(35, 76, 190, 0.9)' } ]}>
+					<Image source={require('../assets/run.png')} style={[ styles.buttonImg, { marginLeft: 9 } ]} />
+					<Text style={styles.buttonText}>跑腿</Text>
+				</TouchableOpacity>
+			</View>
 		</ImageBackground>
 	);
 };
@@ -30,8 +54,40 @@ const styles = StyleSheet.create({
 		},
 		shadowOpacity: 0.27,
 		shadowRadius: 4.65,
-
-		elevation: 6
+		elevation: 6,
+		flex: 1,
+		justifyContent: 'space-evenly',
+		alignItems: 'center',
+		flexDirection: 'row',
+		flexShrink: 0,
+		minHeight: 100
+	},
+	button: {
+		height: 50,
+		width: 50,
+		borderRadius: 50 / 2,
+		flex: 1,
+		flexShrink: 0,
+		maxHeight: 50,
+		maxWidth: 50,
+		marginBottom: 10
+	},
+	buttonImg: {
+		height: 30,
+		width: 30,
+		marginLeft: 8,
+		marginTop: 8
+	},
+	buttonText: {
+		marginTop: 15,
+		marginLeft: 10,
+		fontSize: 13,
+		width: 100
+	},
+	buttonLongText: {
+		marginTop: 15,
+		fontSize: 13,
+		width: 100
 	}
 });
 
